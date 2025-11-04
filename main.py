557d29093bc61099536541be80c07c96e377f0be
Thiago Marques <thiiagoo106@gmail.com>
2025-11-04 20:21:27 -0300

    feat: implementa menu usuário e menu consulta

--- main.py
+++ main.py
@@ -1,34 +1,74 @@
 import modules.usuarios as usuarios
+import modules.consultas as consultas
 
 def exibir_menu_usuario():
       while True:
-            print("\n==== MENU ====")
+            print("==== MENU PRINCIPAL ====\n")
+            print("1. Gerenciar Usuários")
+            print("2. Gerenciar Consultas")
+            print("0. Sair")
+            opcao_principal = input("Escolha uma opção: ")
+
+            if opcao_principal == "1":
+                  menu_usuarios()
+            elif opcao_principal == "2":
+                  menu_consultas()
+            elif opcao_principal == "0":
+                  print("\n👋 Encerrando o programa...")
+                  break
+
+def menu_usuarios():
+      while True:
+            print("\n==== MENU USUÁRIOS ====")
             print("1. Cadastrar usuarios")
             print("2. Listar usuarios")
             print("3. Editar usuario")
             print("4. Deletar usuario")
-            print("0. Sair")
+            print("0. Retornar ao menu principal")
 
-            
-            try:
-                  opcao = int(input("Escolha uma opção: " ))
-                  if opcao == 1:
-                        usuarios.cadastrar_usuarios()
-                  elif opcao == 2:
-                        usuarios.listar_usuarios()
-                  elif opcao == 3:
-                        print("Em breve")
-                  elif opcao == 4:
-                        print("Em breve")
-                  elif opcao == 0:
-                        print("\n👋 Encerrando o programa...")
-                        break
-                  else:
-                        print("Opção invalida")
 
+            opcao = input("Escolha uma opção: " )
+            if opcao == "1":
+                  usuarios.cadastrar_usuarios()
+            elif opcao == "2":
+                  usuarios.listar_usuarios()
+            elif opcao == "3":
+                  print("Em breve")
+            elif opcao == "4":
+                  print("Em breve")
+            elif opcao == "0":
+                  print("\n👋 Encerrando o programa...")
+                  break
+            else:
+                  print("Opção invalida")
                   input("\nAperte enter para continuar...")
 
-            except ValueError:
-                  print("\nInforme uma opção valida")
+      return
+
+def menu_consultas():
+      while True:
+            print("\n==== MENU CONSULTAS ====")
+            print("1. Cadastrar consulta")
+            print("2. Listar consulta")
+            print("3. Editar consulta")
+            print("4. Deletar consulta")
+            print("0. Sair")
+
+            opcao = int(input("Escolha uma opção: " ))
+            if opcao == 1:
+                  consultas.cadastrar_consulta()
+            elif opcao == 2:
+                  print("Em breve")
+            elif opcao == 3:
+                  print("Em breve")
+            elif opcao == 4:
+                  print("Em breve")
+            elif opcao == 0:
+                  print("\n👋 Encerrando o programa...")
+                  break
+            else:
+                  print("Opção invalida")
+
+            input("\nAperte enter para continuar...")
 
 exibir_menu_usuario()
